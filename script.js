c7bc31e5f2240735f4d977ef2be568998e553fe2
Yousaf <78146910+Yousaf-Butt@users.noreply.github.com>
2021-06-25 11:57:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function makePizza() {
+    console.log("Make the dough");
+    console.log("Create the sauce");
+    console.log("Add fresh toppings");
+    console.log("Put pizza in oven");
+    console.log("Get pizza out of the oven");
+}
+
+function makeSushi() {
+    console.log("Spread rice on nori");
+    console.log("Add fillings");
+    console.log("Roll sushi");
+    console.log("Cut the sushi");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
